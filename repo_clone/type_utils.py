cf4fb386503937c923d5a5e09883920401535e91
Rahul Gupta <rahulgupta@L2106.local>
2026-02-16 01:08:29 +0530

    Autonomous AI fix for repo_clone/type_utils.py

--- repo_clone/type_utils.py
+++ repo_clone/type_utils.py
@@ -1,4 +1,9 @@
 def add_one(x):
-    return x + 1
+    if isinstance(x, int):
+        return x + 1
+    elif isinstance(x, str):
+        return x + "1"
+    else:
+        raise TypeError("Unsupported type")
 
 print(add_one("hello"))
